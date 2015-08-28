521d1ede9bf61c1443d4ef771acb5626dca38c29
Yishen Chen <cyt046@gmail.com>
2015-08-28 18:19:00 -0500

    first commit

---
+++ ga4gh.py
@@ -0,0 +1,125 @@
+from __future__ import division
+import gevent.monkey; gevent.monkey.patch_all()
+import gevent
+import gevent.queue
+import requests
+import json
+from collections import defaultdict
+from snps import COORDINATES
+from config import GOOGLE_API_KEY
+
+
+OKG = '10473108253681171589' 
+CONTENT_TYPE_HEADER = {'Content-Type': 'application/json; charset=UTF-8'} 
+# NOTE only google's Variant API works properly
+REPOSITORIES = {
+   'google': 'https://www.googleapis.com/genomics/v1beta2',
+   'ncbi': 'http://trace.ncbi.nlm.nih.gov/Traces/gg',
+   'ebi': 'http://193.62.52.16',
+   'ensembl': 'http://grch37.rest.ensembl.org/ga4gh'
+}
+# signal for worker thread being done
+DONE = None
+
+
+def search(endpoint, repo_id, **data):
+    api_base = REPOSITORIES[repo_id]
+    resp = requests.post('%s/%s/search?key=%s'% (api_base, endpoint, GOOGLE_API_KEY),
+            data=json.dumps(data),
+            headers=CONTENT_TYPE_HEADER)
+    if resp.status_code != 200:
+        raise Exception('Search on %s failed with arguments: %s -- %s'% (resp.url, data, resp.text))
+    else:
+        return resp.json()
+
+
+def matches(a, b):
+    '''
+    check if two genotypes (array of bases, e.g. ['AGT', 'A']) are the same 
+    '''
+    if len(a) != len(b):
+        return False
+    a = sorted(a)
+    b = sorted(b)
+    for i in xrange(len(a)):
+        if a[i] != b[i]:
+            return False 
+    return True
+
+
+def pct(a, b):
+    '''
+    percentage of a/b
+    '''
+    return '%.2f%%'% (a/b*100)
+
+
+def execute_search(queue, *args, **kwargs):
+    '''
+    execute search and shuff result into queue (if there's a result)
+    '''
+    try:
+        while True:
+            resp = search(*args, **kwargs)
+            queue.put(resp)
+            pageToken = resp.get('nextPageToken')
+            if pageToken is not None:
+                kwargs['pageToken'] = pageToken 
+            else:
+                queue.put(DONE)
+                break
+    except:
+        queue.put(DONE)
+        pass
+
+
+def search_variants(genotypes, dataset, repo_id, callSetIds=None):
+    '''
+    yield genotypes within a dataset
+
+    :param genotypes: dictionary mapping a variant name to a genotype
+    :yield: GAVariant
+    '''
+    rsids_by_coords = {}
+    rsids = set()
+    for rsid in genotypes:
+        rsids.add(rsid)
+        coord = COORDINATES[rsid]
+        chrom = coord['chromosome']
+        end = coord['pos']
+        start = end - 1
+        rsids_by_coords['%s:%s-%s'% (chrom, start, end)] = rsid
+
+    variantset_search_resp = search('variantsets', repo_id, datasetIds=[dataset])
+    variant_set_ids = [vs['id'] for vs in variantset_search_resp['variantSets']]
+    # queue to receive search responses
+    search_resps = gevent.queue.Queue()
+    # dispatch concurrent API calls
+    variant_searches = [
+        gevent.spawn(execute_search,
+            search_resps,
+            'variants',
+            variantSetIds=[vsid],
+            referenceName=COORDINATES[rsid]['chromosome'],
+            start=COORDINATES[rsid]['pos']-1,
+            end=COORDINATES[rsid]['pos'],
+            repo_id=repo_id)
+        for rsid in genotypes 
+        for vsid in variant_set_ids
+    ] 
+    num_finished = 0
+    result = []
+    while num_finished < len(variant_searches):
+        resp = search_resps.get()
+        if resp == DONE:
+            num_finished += 1
+            continue
+
+        for variant in resp['variants']: 
+            rsid = rsids_by_coords.get('%s:%s-%s'% (
+                variant['referenceName'],
+                variant['start'],
+                variant['end']))
+            if rsid is not None:
+                result.append((rsid, variant))
+    return result, variant_set_ids
---
+++ web.py
@@ -0,0 +1,125 @@
+import gevent.monkey; gevent.monkey.patch_all()
+import gevent
+from flask import Flask, jsonify, request, render_template, session, url_for, redirect
+from urllib import urlencode
+import json
+import requests
+import ga4gh
+import config
+import snps
+
+app = Flask(__name__)
+app.secret_key = config.SECRET_KEY
+GENOTYPES = {snp: snp_data['Code'] for snp, snp_data in snps.DATA.iteritems()} 
+
+
+def get_access_token(auth_code):
+    '''
+    exchange `code` with `access token`
+    '''
+    exchange_data = {
+        'code': auth_code,
+        'redirect_uri': config.REDIRECT_URI,
+        'client_id': config.CLIENT_ID,
+        'grant_type': 'authorization_code'
+    }
+    resp = requests.post(config.AUTH_BASE+'/token', data=exchange_data)
+    return resp.json()
+
+
+def upload_seq(seq, access_token): 
+    resp = requests.post('%s/Sequence?_format=json'% config.API_BASE,
+            data=json.dumps(seq), 
+            headers={'Authorization': 'Bearer %s'% access_token})
+
+
+@app.route('/load')
+def load(): # load appropriate data from GA4GH
+    callset_id = sample_id = session['sample_id']
+    variants, variantset_ids = ga4gh.search_variants(
+            GENOTYPES,
+            ga4gh.OKG,
+            callSetIds=[sample_id],
+            repo_id='google')
+    pid = session['patient'] 
+    uploads = []
+    for rsid, variant in variants:
+        coord = snps.COORDINATES[rsid]
+        interp = 'positive' if float(snps.DATA[rsid]['Risk']) > 1 else 'negative'
+        seq = {
+            'resourceType': 'Sequence',
+            'chromosome': {'text': coord['chromosome']},
+            'start': coord['pos']-1,
+            'end': coord['pos'], 
+            'genomeBuild': {'text': 'GRCh37'},
+            'type': 'DNA',
+            'patient': {'reference': '/Patient/%s' % pid},
+            'variation': {'text': rsid},
+            'species': {'text': 'Homo sapiens'},
+            'analysis': [{
+                'target': {'text': snps.DATA[rsid]['disease']},
+                'type': {'text': 'Disease Risk Analysis'},
+                'interpretation': {'text': interp},
+                'confidence': 'reviewing'
+            }],
+            'gaRepository': ga4gh.REPOSITORIES['google'],
+            'gaVariantSet': variantset_ids,
+            'gaCallSet': callset_id
+        }
+        uploads.append(gevent.spawn(upload_seq, seq, session['access_token']))
+    for g in uploads:
+        g.join()
+    return redirect('%s/Sequence' % config.API_BASE)
+
+
+@app.route('/recv-redirect')
+def recv_redirect():
+    credentials = get_access_token(request.args['code'])
+    session.update(credentials) 
+    return redirect(url_for('load')) 
+
+
+@app.route('/prompt-select-sample')
+def prompt_select_sample():
+    return render_template('select.html')
+
+
+@app.route('/fhir-app/launch.html')
+def launch():
+    if 'selected' not in request.args:
+        session['launch_args'] = json.dumps(request.args)
+        return redirect(url_for('prompt_select_sample'))
+
+    redirect_args = {
+        'scope': ' '.join(config.SCOPES+['launch:'+request.args['launch']]),
+        'client_id': config.CLIENT_ID,
+        'redirect_uri': config.REDIRECT_URI,
+        'response_type': 'code'}
+    return redirect('%s/authorize?%s'% (config.AUTH_BASE, urlencode(redirect_args)))
+
+
+@app.route('/select-sample')
+def select_sample():
+    launch_args = json.loads(session['launch_args'])
+    launch_args['selected'] = ''
+    session['sample_id'] = request.args['sample_id']
+    return redirect('%s?%s'% (
+        url_for('launch'),
+        urlencode(launch_args)))
+
+
+@app.route('/callsets')
+def get_callsets(): 
+    vset_search = ga4gh.search('variantsets', datasetIds=[ga4gh.OKG], repo_id='google') 
+    vset_id = vset_search['variantSets'][0]['id']
+    callset_search = ga4gh.search(
+            'callsets',
+            variantSetIds=[vset_id],
+            repo_id='google',
+            pageSize=10,
+            **request.args)
+    return jsonify(callset_search)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
